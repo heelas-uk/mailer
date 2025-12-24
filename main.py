ac5ed7adb02bbc1e23d9550a4ccca5ba53e83d47
Alfred Heelas <180876265+heelas-uk@users.noreply.github.com>
2025-12-24 13:19:49 +0000

    Chnage to an api because SMTP wouldn't wokrl

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import io
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import email.utils
+from datetime import datetime
+from client_timezone import client_timezone
+from zoneinfo import ZoneInfo
+import requests
+timezone = client_timezone(key="client-timezone")
 
 def login_screen():
     st.subheader("Please log in.")
@@ -30,6 +35,10 @@ else:
             csv_reader = csv.DictReader(io.StringIO(mail_data))
             emails = [row['email'] for row in csv_reader if 'email' in row and row['email']]
             names = [row['names'] for row in csv_reader if 'names' in row and row['names']]
+            scheduled = st.datetime_input(label="When would you like to schedule the email? To send ASAP leave blank", min_value="now", value=None, format="DD/MM/YYYY")
+            if scheduled is not None:
+                local = scheduled.replace(tzinfo=timezone)
+                utc_shedule = local.astimezone(ZoneInfo("UTC"))
             st.write("Emails to be sent to:")
             for i in emails:
                 st.code(i, language=None)
@@ -45,25 +54,42 @@ else:
                 # EMAIL TIME
                 try: 
                     for i, n in zip(emails, names):
-                        msg = MIMEMultipart()
-                        msg['From'] = from_email
-                        msg['To'] = i
-                        msg['Subject'] = subject
-                        msg['Message-ID'] = email.utils.make_msgid(domain="heelas.uk")
-                        msg['Reply-To'] = behalf_of_email
-                        msg['Keywords'] = "hackclub, hc, Hack Club, "+behalf_of_name+", "+behalf_of_name
-                        msg["Comments"] = "This email was sent by a third party please do no reply here"
                         # Replace placeholders in HTML
                         message = html_template.replace("[body]", body)
-                        message = message.replace("[behalf_of_name]", behalf_of_name)
-                        message = message.replace("[behalf_of_email]", behalf_of_email)
+                        message = message.replace("[behalf_of_name]", str(behalf_of_name))
+                        message = message.replace("[behalf_of_email]", str(behalf_of_email))
                         message = message.replace("[name]", n)
+                        if scheduled is not None:
+                            headers = {
+                                    "api-key": st.secrets['brevo_api'],
+                                    "replyTo.email": "21heelasa@sta.cc",
+                                    "replyTo.name": "Alfred Heelas",
+                                    "tags": f"hackclub, hc, Hack Club, {behalf_of_email}, {behalf_of_name}",
+                                    "sender.email": "void@sta.hackclub.uk",
+                                    "sender.name": f"STA Hak Club {behalf_of_name}",
+                                    "to.email": i,
+                                    "to.name": n,
+                                    "subject": subject,
+                                    "scheduledAt": utc_shedule,
+                                    "htmlContent": message
+                                }
+                        else:
+                            headers = {
+                                    "api-key": st.secrets['brevo_api'],
+                                    "replyTo.email": "21heelasa@sta.cc",
+                                    "replyTo.name": "Alfred Heelas",
+                                    "tags": f"hackclub, hc, Hack Club, {behalf_of_email}, {behalf_of_name}",
+                                    "sender.email": "void@sta.hackclub.uk",
+                                    "sender.name": f"STA Hak Club {behalf_of_name}",
+                                    "to.email": i,
+                                    "to.name": n,
+                                    "subject": subject,
+                                    "htmlContent": message
+                                }
 
-                        msg.attach(MIMEText(message, "html"))
-                        mailer= smtplib.SMTP(smtp_server, 587)
-                        mailer.login(user, password)
-                        mailer.sendmail(from_email, i, msg.as_string() )
+
                     st.success("Email sent")
+                except requests.exceptions.RequestException as e:
+                    st.error(f"Failed to send email: {str(e)}")
                 except Exception as e:
-                    st.error(f"Mail failed: {e}")
-    st.button("Log out", on_click=st.logout)
+                    st.error(f"An unexpected error occurred: {str(e)}")
